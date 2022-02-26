71448dc878362316ae27689e8ba310c2faad545e
Luillin Escobar <luillin.escobar@hotmail.com>
2022-02-25 20:57:00 -0500

    NTV001 se agrega ejemplo de condicional

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#comentario
-print("que miedo la guerra");
 
 nivelAgua= int(input("Digite la cantidad de agua de la represa: "));
-
-print(f"el nivel de agua es: ${nivelAgua}");
-
-print('oee');
+if(nivelAgua<=200):
+    print(f"el nivel de agua es: ${nivelAgua}. No tengo agua");
+elif(nivelAgua>=200 and nivelAgua <= 350):
+    print(f"el nivel de agua es: ${nivelAgua}. Todo bien energia corriente");
+else:
+    print(f"el nivel de agua es: ${nivelAgua}. Estamos full");
