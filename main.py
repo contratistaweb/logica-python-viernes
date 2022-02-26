4cfc3388f963673415c7223c784a674ae310c117
Luillin Escobar <luillin.escobar@hotmail.com>
2022-02-25 19:23:59 -0500

    NTV002 se declara variable de nivel de agua y se imprime en consola

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #comentario
+from cmd import PROMPT
+
+
 print("que miedo la guerra");
+
+nivelAgua= int(input("Digite la cantidad de agua de la represa: "));
+
+print(f"el nivel de agua es: ${nivelAgua}");
